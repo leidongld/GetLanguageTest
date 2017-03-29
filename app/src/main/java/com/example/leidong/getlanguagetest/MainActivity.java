d7782080591d65f279f0d9595721c905b9f37a57
Lei Dong <394900130@qq.com>
2017-03-29 14:25:42 +0800

    Some changes

--- app/src/main/java/com/example/leidong/getlanguagetest/MainActivity.java
+++ app/src/main/java/com/example/leidong/getlanguagetest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.leidong.getlanguagetest;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +11,25 @@ import android.widget.TextView;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    //getCountry（）
+    private TextView textView1;
+    //getLanguage()
+    private TextView textView2;
+    //getDisplayCountry()
+    private TextView textView3;
+    //getDisplayLanguage()
+    private TextView textView4;
+    //getDisplayName()
+    private TextView textView5;
+    //getDisplayScript()
+    private TextView textView6;
+    //getDisplayVariant()
+    private TextView textView7;
+    //getDisplayCountries()
+    private TextView textView8;
+    //getDisplayLangueges
+    private TextView textView9;
+    //Button
     private Button button;
 
     @Override
@@ -21,13 +41,31 @@ public class MainActivity extends AppCompatActivity {
         initView();
 
         button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 //获取当前系统的语言
                 String languageInfo = Locale.getDefault().getLanguage().toString();
                 //获取当前语言对应的国家
-                String country = getResources().getConfiguration().locale.getCountry();
-                textView.setText(languageInfo + "\n" + country);
+                textView1.setText(Locale.getDefault().getCountry());
+                textView2.setText(Locale.getDefault().getLanguage());
+                textView3.setText(Locale.getDefault().getDisplayCountry());
+                textView4.setText(Locale.getDefault().getDisplayLanguage());
+                textView5.setText(Locale.getDefault().getDisplayName());
+                textView6.setText(Locale.getDefault().getISO3Country());
+                textView7.setText(Locale.getDefault().getISO3Language());
+                String[] strs1 = Locale.getISOCountries();
+                StringBuilder sb1 = new StringBuilder();
+                for(int i = 0; i < strs1.length; i++){
+                    sb1.append(strs1[i] + "  ");
+                }
+                textView8.setText(sb1.toString());
+                String[] strs2 = Locale.getISOLanguages();
+                StringBuilder sb2 = new StringBuilder();
+                for(int i = 0; i < strs2.length; i++){
+                    sb2.append(strs2[i] + "  ");
+                }
+                textView9.setText(sb2.toString());
             }
         });
     }
@@ -36,7 +74,15 @@ public class MainActivity extends AppCompatActivity {
      * 获取控件
      */
     private void initView() {
-        textView = (TextView) findViewById(R.id.textView);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4 = (TextView) findViewById(R.id.textView4);
+        textView5 = (TextView) findViewById(R.id.textView5);
+        textView6 = (TextView) findViewById(R.id.textView6);
+        textView7 = (TextView) findViewById(R.id.textView7);
+        textView8 = (TextView) findViewById(R.id.textView8);
+        textView9 = (TextView) findViewById(R.id.textView9);
         button = (Button) findViewById(R.id.button);
     }
 }
